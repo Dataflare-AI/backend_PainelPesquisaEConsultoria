e1b4c6979e4c376f382bb0a800d9288f5dd895c2
dudubarbaro <dudubarbaro>
2023-11-15 01:13:05 -0300

    Add description field to FileUploadAPI model and update its string representation

--- fusion/models.py
+++ fusion/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class FileUploadAPI(models.Model):
     file = models.FileField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.uploaded_on.date()
+        # return self.uploaded_on.date()
+        return f"{self.file.name} uploaded on {self.uploaded_on.date()}"
