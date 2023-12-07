00299ae232ab1d72c8427253e603fde958252244
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-07 07:58:36 -0300

    Update IP address in .env file

---
+++ actions/import_xslx.py
@@ -0,0 +1,10 @@
+# import xlrd
+# from fusion.models import ExcelFile
+
+# # filename = "media/excel/membros_std.xlsx"
+
+
+# def import_xslx(filename):
+#     workbook = xlrd.open_workbook(filename)
+#     sheet = workbook.sheet_by_index(0)
+#     fiels = ""
